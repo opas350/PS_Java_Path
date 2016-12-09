ba7e758d6a461e59c6f41dec879e928265bf4495
opas350 <opas350@gmail.com>
2016-12-08 22:13:35 -0600

    Add remaining code for part1

---
+++ Beginner/1_Java_Fundamentals/10_Creating_Abstract_Relationships_with_Interfaces/Demo1_Declare_And_Implement/src/com/opas350/ps/CalcEngine/DynamicHelper.java
@@ -0,0 +1,47 @@
+package com.opas350.ps.CalcEngine;
+
+/**
+ * Created by Javier on 12/8/2016.
+ */
+public class DynamicHelper {
+
+    private MathProcessing[] handlers;
+
+    public DynamicHelper(MathProcessing[] handlers) {
+        this.handlers = handlers;
+    }
+
+    public String process(String statements) {
+        // IN: add 1.0 2.0
+        // OUT: 1.0 + 2.0 = 3.0
+
+        String[] parts = statements.split(MathProcessing.SEPARATOR);
+        String keyword = parts[0]; // add
+
+        MathProcessing theHandler = null;
+
+        for(MathProcessing handler : handlers) {
+            if(keyword.equalsIgnoreCase(handler.getKeyword())){
+                theHandler = handler;
+                break;
+            }
+        }
+
+        double leftVal = Double.parseDouble(parts[1]);
+        double rightVal = Double.parseDouble(parts[2]);
+
+        double result = theHandler.doCalculation(leftVal, rightVal);
+
+        StringBuilder sb = new StringBuilder(20);
+        sb.append(leftVal);
+        sb.append(' ');
+        sb.append(theHandler.getSymbol());
+        sb.append(' ');
+        sb.append(rightVal);
+        sb.append(" = ");
+        sb.append(result);
+
+        return sb.toString();
+
+    }
+}
---
+++ Beginner/1_Java_Fundamentals/11_StaticMember_NestedTypes_AnonymousClasses/Demo1_Declare_And_Implement/src/com/opas350/ps/CalcEngine/Adder.java
@@ -0,0 +1,38 @@
+package com.opas350.ps.CalcEngine;
+
+/**
+ * Created by Javier on 11/29/2016.
+ */
+public class Adder extends CalculateBase implements MathProcessing {
+
+    public Adder() {}
+
+    public Adder(double leftVal, double rightVal) {
+        super(leftVal, rightVal);
+    }
+
+    @Override
+    public void calculate() {
+        double value = getLeftVal() + getRightVal();
+        setResult(value);
+    }
+
+    @Override
+    public String getKeyword() {
+        return "add";
+    }
+
+    @Override
+    public char getSymbol() {
+        return '+';
+    }
+
+    @Override
+    public double doCalculation(double leftVal, double rightVal) {
+        setLeftVal(leftVal);
+        setRightVal(rightVal);
+        calculate();
+
+        return getResult();
+    }
+}
